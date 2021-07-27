c783748e6d597d9f7fe1ac8c47abb46d2d50ccfe
Ivan Torres <ivantorresmia27@gmail.com>
2021-07-26 17:51:15 -0700

    added buttons and new comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // this is the tag you will use to call the component in other components
   selector: 'app-root',
+  // just telling it to use this HTML file
   templateUrl: './app.component.html',
+  // Just telling it to use this CSS File
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title: string = 'task tracker';
 }
